7f147907f9df8674ed32d32e25da55cc87974ba9
juliakocur <juliakocur@mail.com>
2023-02-20 11:20:06 +0100

    docs:add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+    const bracketsPair = new Map(bracketsConfig);
+    const stack = [];
+    for (const char of str) { 
+      if (bracketsPair.get(stack.at(-1)) == char) { 
+        stack.pop();
+      } else {
+        stack.push(char);
+      }
+    }
+    return stack.length == 0; 
+  }
+  
